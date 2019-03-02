03610d8881895eaadc3876011e54e5935ba0eb71
Ivan <izinovyev42@gmail.com>
2019-03-02 09:57:30 +0300

    Fix Error #2

--- src/main/java/model/Sticker.kt
+++ src/main/java/model/Sticker.kt
@@ -4,7 +4,7 @@ class Sticker(val url: String) {
     val name:String
     get() {
         val leftIndent = "sticker/"
-        val rightIndent = "/ANDROID"
+        val rightIndent = "/android"
         return url.substring(url.indexOf(leftIndent) + leftIndent.length, url.indexOf(rightIndent))
     }
 }
