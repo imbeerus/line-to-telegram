65ae721cf1d7424faded4bb63032d6e8622e71b2
Albert Hsieh <knowlet@users.noreply.github.com>
2019-04-07 17:05:44 +0800

    Fix get Sticker name issue
    The sticker image url can be both android or iPhone, it's better to use other ways to do things right.

--- src/main/java/model/Sticker.kt
+++ src/main/java/model/Sticker.kt
@@ -3,8 +3,7 @@ package model
 class Sticker(val url: String) {
     val name:String
     get() {
-        val leftIndent = "sticker/"
-        val rightIndent = "/android"
-        return url.substring(url.indexOf(leftIndent) + leftIndent.length, url.indexOf(rightIndent))
+        val r = Regex("sticker/(\\d+)/")
+        return r.find(url)!!.groupValues[1]
     }
 }
